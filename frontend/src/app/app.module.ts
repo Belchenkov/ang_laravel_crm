d9f24c6d02f3926604873e3652eeb12995c2997d
Aleksey Belchenkov <u608110@gmail.com>
2021-07-31 09:57:08 +0400

    Base structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./modules/material/material.module";
+import { LayoutComponent } from './components/layout/layout/layout.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
+import { PreloaderComponent } from './components/layout/preloader/preloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavigationComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
